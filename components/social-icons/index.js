26d61a908b098b345384e277fba81c9e83695454
Chi Nguyen <c.nguyen.prog@gmail.com>
2021-11-13 22:04:33 +0100

    update ext. links, add xing icon

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -4,6 +4,7 @@ import Facebook from './facebook.svg'
 import Youtube from './youtube.svg'
 import Linkedin from './linkedin.svg'
 import Twitter from './twitter.svg'
+import Xing from './xing.svg'
 
 // Icons taken from: https://simpleicons.org/
 
@@ -14,6 +15,7 @@ const components = {
   youtube: Youtube,
   linkedin: Linkedin,
   twitter: Twitter,
+  xing: Xing,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
@@ -24,14 +26,14 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
 
   return (
     <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
+      className="group text-sm text-gray-500 transition hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-800 p-1 rounded"
       target="_blank"
       rel="noopener noreferrer"
       href={href}
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={`fill-current text-gray-700 dark:text-gray-200 group-hover:text-blue-500 dark:group-hover:text-blue-400 h-${size} w-${size}`}
       />
     </a>
   )
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,24 +1,25 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'cnguyen.de',
+  author: 'Chi Nguyen',
+  headerTitle: 'cnguyen.de',
+  description: 'Chi Nguyen personal blog',
   language: 'en-us',
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://cnguyen.de',
+  siteRepo: 'https://github.com/cnguyen-de/nextjs-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'c.nguyen.prog@gmail.com',
+  github: 'https://github.com/cnguyen-de',
+  twitter: 'https://twitter.com/chistweets',
+  facebook: '',
+  youtube: '',
+  linkedin: 'https://www.linkedin.com/in/chi-nguyen-a950b5a1/',
+  xing: 'https://www.xing.com/profile/Chi_Nguyen23',
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'cnguyen.de', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
